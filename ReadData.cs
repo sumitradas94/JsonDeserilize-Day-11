16d46e3fb2cea0e8ae15234d0fb18f27c604e35e
sumitradas94 <sumitradas94@gmail.com>
2023-01-06 12:12:06 +0530

    Read all the deatils

---
+++ ReadData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class ReadData
+    {
+        public InventoryDetails Read(string path)
+        {
+        using (StreamReader file = new StreamReader(path)) { 
+                try
+                {
+                    string json=file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<InventoryDetails>(json);
+                }catch(Exception e)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
