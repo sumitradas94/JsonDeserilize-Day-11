bbcbb185616a173d1cdc3562c46bf62d59ccb510
sumitradas94 <sumitradas94@gmail.com>
2023-01-06 12:09:57 +0530

    Inventorydetails

---
+++ InventoryDetails.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class InventoryDetails
+    {
+        // public TypesOfRice typesOfRice;
+        public List<TypesOfRice> typesOfRice;
+        public List<TypesOfPuslses> typesOfPulses;
+        public List<TypesOfWheat> typesOfWheat;
+
+    }
+
+    public class TypesOfRice
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+
+    public class TypesOfPuslses
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+
+    public class TypesOfWheat
+    {
+        public string name;
+        public int weight;
+        public int price;
+
+        
+    }
+}
+
+
+    
+
