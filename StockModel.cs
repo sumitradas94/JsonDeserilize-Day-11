6c1b5e07ec43ba3c29c510ff0d0f1ddb2780491e
sumitradas94 <120413479+sumitradas94@users.noreply.github.com>
2023-01-18 10:37:08 +0530

    Add files via upload

---
+++ StockModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class StockModel
+    {
+        public List<CommonStocks> commonStocks;
+        public List<PreferredStocks> preferredStocks;
+        public List<HybridStocks> hybridStocks;
+
+    }
+    public class Commpanyshares
+    {
+        public string symbol { get; set; }
+        public int numberOfshares { get; set; }
+        public string DateTime { get; set; }
+    }
+
+    public class PreferredStocks
+    {
+        public string sharename;
+        public int numberOfShare;
+        public int sharePrice;
+    }
+
+    public class CommonStocks
+    {
+        public string sharename;
+        public int numberOfShare;
+        public int sharePrice;
+    }
+
+    public class HybridStocks
+    {
+        public string sharename;
+        public int numberOfShare;
+        public int sharePrice;
+    }
+
+}
+    
